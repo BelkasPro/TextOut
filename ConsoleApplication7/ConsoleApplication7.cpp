b6e3c58f999c161bc69d5e22edd6aeddd153df70
1 <1@DESKTOP-24LDP65>
2024-04-30 16:33:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <Windows.h>
+#define MYRGB(r,g,b) (r | g << 8 | b << 16)
+using namespace std;
+int main()
+{
+
+
+	LPCWSTR lptext = L"Your Text"; //You can make your own text
+	while (true) {
+
+		HDC hdc;
+		hdc = GetWindowDC(GetDesktopWindow());
+		int sw = GetSystemMetrics(0);
+		int sh = GetSystemMetrics(1);
+		SetTextColor(hdc, MYRGB(0, 0, 0)); //You can make your text color
+		SetBkColor(hdc, MYRGB(255, 255, 255)); //You can make your own background color
+		TextOutW(hdc, rand() % sw, rand() % sh, lptext, wcslen(lptext));
+		TextOutW(hdc, rand() % sw, rand() % sh, lptext, wcslen(lptext));
+		TextOutW(hdc, rand() % sw, rand() % sh, lptext, wcslen(lptext));
+	}
+}
